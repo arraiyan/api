b38417d346c9ab4d9b72f4c9254916bf9354af28
arraiyan <77921265+arraiyan@users.noreply.github.com>
2021-10-26 11:00:50 +0600

    u

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def entry_point():
-    return jsonify(main.data())
+    return jsonify({'holder':1552})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
