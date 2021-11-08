7f566e05aa82c405e011515eff230ce633ceb31a
arraiyan <77921265+arraiyan@users.noreply.github.com>
2021-11-08 11:52:57 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask , jsonify
+from flask import Flask , jsonify ,render_template
 import main
 app = Flask(__name__)
 
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def entry_point():
     return jsonify(main.data())
 
+
+
+@app.route('/live_progress')
+def progress_page():
+    return render_template("index.html")
+
+
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True,port=8000)
